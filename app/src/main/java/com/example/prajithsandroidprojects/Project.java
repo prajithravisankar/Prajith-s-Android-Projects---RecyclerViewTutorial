d0f55659c816b0f009995be4a9dde90233c0705f
prajithravisankar <prajithravisankar@gmail.com>
2024-08-11 17:34:08 -0700

    2nd commit - created Project.java class.

---
+++ app/src/main/java/com/example/prajithsandroidprojects/Project.java
@@ -0,0 +1,14 @@
+package com.example.prajithsandroidprojects;
+
+public class Project {
+    String name;
+    String description;
+    int image;
+
+    // constructor
+    public Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
