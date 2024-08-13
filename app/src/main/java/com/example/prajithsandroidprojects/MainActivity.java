ffcc3124e1358e451ae6d5a9c3016e4e902470a5
prajith <prajithravisankar@gmail.com>
2024-08-13 13:01:43 -0700

    4th commit - Added id to Recycler view. Started with ProjectAdapter.

--- app/src/main/java/com/example/prajithsandroidprojects/MainActivity.java
+++ app/src/main/java/com/example/prajithsandroidprojects/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
         Project[] projects = {
                 new Project("getting started app",
                         "a simple getting started app for learning android studio setup",
